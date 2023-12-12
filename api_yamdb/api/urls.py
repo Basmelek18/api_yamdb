b655592f3a9c0186ae8a94be038e935c244d8203
Igor Dolgonosov <garik78@bk.ru>
2023-12-12 13:18:41 +0100

    created_urls_for_comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,31 @@
+from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from .views import ReviewViewSet
+
+from .views import (
+    ReviewViewSet,
+    CommentViewSet,
+    SignupView,
+    TokenTakenView
+)
 
 
-router = SimpleRouter()
+router_v1 = SimpleRouter()
 
-router.register(
+router_v1.register(
     r'titles/(?P<post_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
+router_v1.register(
+    r'titles/(?P<post_id>\d+)/reviews/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
+
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/auth/signup/', SignupView.as_view(), name='token_obtain_pair'),
+    path('v1/auth/token/', TokenTakenView.as_view(), name='token_refresh'),
+]
