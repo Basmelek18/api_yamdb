f43a2949e9020549b7d968afed46660d0b41c2c9
Igor Dolgonosov <garik78@bk.ru>
2023-12-14 00:13:13 +0100

    bugs/permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,31 +3,38 @@ from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_admin
-            or request.user.is_staff
-            or request.user.is_superuser
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+            and request.user
         )
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and (
-            request.user.is_staff
-            or request.user.is_admin
-            or request.user.is_superuser
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+            and request.user
         )
 
 
 class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user and request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
+
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and (
+        return (
             request.method in permissions.SAFE_METHODS
             or request.user == obj.author
-            or request.user.is_staff
             or request.user.is_admin
-            or request.user.is_superuser
         )
 
 
 class ReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS
