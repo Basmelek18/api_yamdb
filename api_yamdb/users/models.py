3044a4d85d3686c71a69d38635199222cc95a0ec
Ispaniuk Viacheslav <basmelek18@gmail.com>
2023-12-14 22:41:41 +0300

    enum is back baby

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,17 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# from .enums import UserRole
-
-ADMIN = 'admin'
-USER = 'user'
-MODERATOR = 'moderator'
-
-
-ROLE_CHOICES = (USER, MODERATOR, ADMIN)
+from .enums import UserRole
 
 
 class UserYamDb(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('first_name', 'last_name', 'username')
+
     email = models.EmailField(
         verbose_name='Электронная почта',
         unique=True,
@@ -39,30 +35,29 @@ class UserYamDb(AbstractUser):
     role = models.CharField(
         verbose_name='Роль',
         max_length=20,
-        choices=ROLE_CHOICES,
-        default=USER
+        choices=UserRole.choices(),
+        default='user'
     )
     confirmation_code = models.CharField(max_length=6)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('first_name', 'last_name', 'username')
+    @property
+    def is_admin(self):
+        return self.role == (
+                UserRole.ADMIN.value
+                or self.is_superuser
+                or self.is_staff
+        )
+
+    @property
+    def is_moderator(self):
+        return self.role == UserRole.MODERATOR.value
+
+    @property
+    def is_user(self):
+        return self.role == UserRole.USER.value
 
     class Meta:
         verbose_name = ''
         verbose_name_plural = ''
         ordering = ('last_name', 'first_name')
 
-    def __str__(self):
-        return self.username
-
-    @property
-    def is_admin(self):
-        return self.role == ADMIN
-
-    @property
-    def is_moderator(self):
-        return self.role == MODERATOR
-
-    @property
-    def is_user(self):
-        return self.role == USER
